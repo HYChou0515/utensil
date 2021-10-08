711627ff3b16abb1ae27ffa0429069eb01739818
Chou Hung-Yi <hychou.svm@gmail.com>
2021-10-08 23:32:45 +0800

    publish test-pypi in workflow

--- test/constant/test_constant.py
+++ test/constant/test_constant.py
@@ -2,8 +2,11 @@ import os
 import unittest as ut
 from importlib import reload
 
+from utensil import constant
+
 
 class TestDefaultConstant(ut.TestCase):
+
     def setUp(self) -> None:
         self.project_root = os.path.dirname(os.path.dirname(__file__))
         try:
@@ -19,32 +22,22 @@ class TestDefaultConstant(ut.TestCase):
             del os.environ["UTENSIL_PROJECT_ROOT"]
 
     def test_PROJECT_ProjectRoot(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(self.project_root, constant.PROJECT["ProjectRoot"])
 
     def test_PROJECT_ProjectName(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(".utensil", constant.PROJECT["ProjectName"])
 
     def test_PROJECT_ProjectAbbr(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(".utensil", constant.PROJECT["ProjectAbbr"])
 
     def test_PROJECT_ProjectState(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("dev", constant.PROJECT["ProjectState"])
 
     def test_PROJECT_ConfigPath(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(
             os.path.join(self.project_root, "utensil.ini"),
@@ -52,40 +45,27 @@ class TestDefaultConstant(ut.TestCase):
         )
 
     def test_HOST_INFO_HostName(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("localhost", constant.HOST_INFO["HostName"])
 
     def test_LOG_Dir(self):
-        from utensil import constant
-
         reload(constant)
-        self.assertEqual(
-            os.path.join(self.project_root, ".utensil", "log"), constant.LOG["Dir"]
-        )
+        self.assertEqual(os.path.join(self.project_root, ".utensil", "log"),
+                         constant.LOG["Dir"])
 
     def test_LOG_Stream(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("info", constant.LOG["Stream"])
 
     def test_LOG_Syslog(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("notset", constant.LOG["Syslog"])
 
     def test_LOG_File(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("info", constant.LOG["File"])
 
     def test_LOG_FilePrefix(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(
             os.path.join(self.project_root, ".utensil", "log", ".utensil.log"),
@@ -93,19 +73,16 @@ class TestDefaultConstant(ut.TestCase):
         )
 
     def test_LOG_Level(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("info", constant.LOG["Level"])
 
     def test_LOG_MaxMessageLen(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("60000", constant.LOG["MaxMessageLen"])
 
 
 class TestModifiedConstant(ut.TestCase):
+
     def setUp(self) -> None:
         self.project_root = os.path.dirname(__file__)
         try:
@@ -131,32 +108,22 @@ class TestModifiedConstant(ut.TestCase):
             del os.environ["UTENSIL_CONFIG"]
 
     def test_PROJECT_ProjectRoot(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(self.project_root, constant.PROJECT["ProjectRoot"])
 
     def test_PROJECT_ProjectName(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("proj_name", constant.PROJECT["ProjectName"])
 
     def test_PROJECT_ProjectAbbr(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("proj_name_proj_abbr", constant.PROJECT["ProjectAbbr"])
 
     def test_PROJECT_ProjectState(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("proj_state", constant.PROJECT["ProjectState"])
 
     def test_PROJECT_ConfigPath(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(
             os.path.join(self.project_root, self.utensil_config),
@@ -164,40 +131,27 @@ class TestModifiedConstant(ut.TestCase):
         )
 
     def test_HOST_INFO_HostName(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("my_host", constant.HOST_INFO["HostName"])
 
     def test_LOG_Dir(self):
-        from utensil import constant
-
         reload(constant)
-        self.assertEqual(
-            os.path.join("proj_name_proj_abbr", "log_test"), constant.LOG["Dir"]
-        )
+        self.assertEqual(os.path.join("proj_name_proj_abbr", "log_test"),
+                         constant.LOG["Dir"])
 
     def test_LOG_Stream(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("notset", constant.LOG["Stream"])
 
     def test_LOG_Syslog(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("info", constant.LOG["Syslog"])
 
     def test_LOG_File(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("notset", constant.LOG["File"])
 
     def test_LOG_FilePrefix(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual(
             os.path.join("proj_name_proj_abbr", "log_test", "proj_name.log123"),
@@ -205,14 +159,10 @@ class TestModifiedConstant(ut.TestCase):
         )
 
     def test_LOG_Level(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("debug", constant.LOG["Level"])
 
     def test_LOG_MaxMessageLen(self):
-        from utensil import constant
-
         reload(constant)
         self.assertEqual("12345", constant.LOG["MaxMessageLen"])
 
