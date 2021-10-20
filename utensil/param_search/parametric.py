424d8f7b234e3e88044d33407f93a878ba9a9023
Chou Hung-Yi <hychou.svm@gmail.com>
2021-10-20 21:11:29 +0800

    parametric seeder added

--- utensil/param_search/parametric.py
+++ utensil/param_search/parametric.py
@@ -2,11 +2,140 @@ from __future__ import annotations
 
 import abc
 import math
+import random
 from collections import Counter
 from dataclasses import dataclass
-from typing import Any, Tuple
+from typing import Any, Tuple, Generator
 
 from utensil.general import warn_left_keys
+
+
+class BaseParametricSeeder(abc.ABC):
+
+    def __init__(self, state=0, size=1, rng=None, max_state=10**10):
+        self._state = state
+        self.rng = random if rng is None else rng
+        self.size = size
+        self.max_state = max_state
+
+    @property
+    def state(self):
+        return self._state
+
+    @abc.abstractmethod
+    def _seeds(self) -> Generator[Tuple[float], None, None]:
+        raise NotImplementedError
+
+    def seeds(self) -> Generator[Tuple[float], None, None]:
+        for rs in self._seeds():
+            for r in rs:
+                if not 0 <= r < 1:
+                    raise ValueError(
+                        f'Returned param should be in range [0, 1), got {r}')
+            yield rs
+            self._state += 1
+            if self.state >= self.max_state:
+                raise ValueError(f'Reached maximum state {self.max_state}')
+
+
+class SimpleParametricSeeder(BaseParametricSeeder):
+    # noinspection PyUnresolvedReferences
+    """A simple implementation of :class:`.BaseParametricSeeder`.
+    >>> from scipy import stats
+    >>> import numpy as np
+    >>> seeds = np.array([s for s, _ in zip(SimpleParametricSeeder(
+    ...     rng=np.random.default_rng(0), size=3
+    ... ).seeds(), range(1000))])
+    >>> k1 = [stats.kstest(
+    ...     seeds[:,i], stats.uniform.cdf, args=(0, 1)
+    ... ).pvalue for i in range(3)]
+    >>> assert min(k1) > 0.05
+    """
+
+    def _seeds(self) -> Generator[Tuple[float], None, None]:
+        while True:
+            yield tuple(self.rng.random() for _ in range(self.size))
+
+
+class MoreUniformParametricSeeder(BaseParametricSeeder):
+    # noinspection PyUnresolvedReferences
+    """A more uniform parametric seeder.
+    For *more* uniform, it is comparing to :class:`.SimpleParametricSeeder`.
+
+    In pure uniform distribution, small sample size may result in *non-uniform*
+    output. For example, there is `18.75%` chance for 5 uniformly distributed
+    variables having range smaller than `0.5`.
+
+    This class provide a *more uniform distributed* variables, though it is not
+    actually uniform distributed. They are not i.i.d, either.
+
+    Variables are generated via the following algorithm.
+
+    At step 1, `x_1` is from U(0, 1).
+    At step 2 and 3, `x_2`, `x_3` is from shuffle(U(0, 0.5), U(0.5, 1)).
+    At step 4 to 7, `x_4` ... `x_7` is from
+        shuffle(U(0, 0.25), ..., U(0.75, 1)).
+    ...
+
+    For multiple variables case, via the following algorithm.
+
+    At step 1, `x_i1` is from U(0, 1) for every `i`.
+    At step 2 and 3, `x_i2`, `x_i3` is from shuffle(U(0, 0.5), U(0.5, 1)).
+        Return shuffle(`x_i2`) in step 2 and shuffle(`x_i3`) in step 3.
+    At step 4 to 7, `x_i4` ... `x_i7` is from
+        shuffle(U(0, 0.25), ..., U(0.75, 1)).
+        Return shuffle(`x_ij`) in step j.
+    ...
+
+    >>> from scipy import stats
+    >>> import numpy as np
+    >>> seeds1 = np.array([s for s, _ in zip(SimpleParametricSeeder(
+    ...     rng=np.random.default_rng(), size=1000
+    ... ).seeds(), range(15))])
+    >>> seeds2 = np.array([s for s, _ in zip(MoreUniformParametricSeeder(
+    ...     rng=np.random.default_rng(), size=1000
+    ... ).seeds(), range(15))])
+    >>> k1 = [stats.kstest(
+    ...     seeds1[:,i], stats.uniform.cdf, args=(0, 1)
+    ... ).pvalue for i in range(1000)]
+    >>> k2 = [stats.kstest(
+    ...     seeds2[:,i], stats.uniform.cdf, args=(0, 1)
+    ... ).pvalue for i in range(1000)]
+    >>> assert max(k1) < max(k2)
+    >>> assert min(k1) < min(k2)
+    >>> assert sorted(k1)[:len(k1)//2] < sorted(k2)[:len(k2)//2]
+    """
+
+    def _random_between(self, a, b):
+        import numpy as np
+        if self.rng is random:
+            return tuple(
+                self.rng.random() * (b - a) + a for _ in range(self.size))
+        if isinstance(self.rng, np.random.Generator):
+            return self.rng.random(size=self.size) * (b - a) + a
+        raise TypeError(f'Non expected type of rng: {type(self.rng).__name__}')
+
+    def _seeds(self) -> Generator[Tuple[float], None, None]:
+        import numpy as np
+        rand_space = []
+        while True:
+            base = 2**int(np.log2(self.state + 1))
+            offset = self.state + 1 - base
+            if offset == 0 or len(rand_space) == 0:
+                linspace = np.linspace(0, 1, base + 1)
+                rand_space = np.array([
+                    self._random_between(
+                        linspace[i],
+                        linspace[i + 1],
+                    ) for i in range(base)
+                ])
+
+                for i in range(self.size):
+                    self.rng.shuffle(rand_space[:, i])
+
+            model_r = tuple(rand_space[offset])
+
+            yield model_r
 
 
 class Parametric(abc.ABC):
@@ -229,6 +358,7 @@ class ExponentialBetweenParam(Parametric):
 
 @dataclass(init=False)
 class ChoicesParam(Parametric):
+    # noinspection PyUnresolvedReferences
     """Uniformly select a choice within given choices.
 
     >>> from scipy import stats
