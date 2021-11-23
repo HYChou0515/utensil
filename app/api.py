b5967875951fcd516beb8d3bc4649e98f8c50996
Chou Hung-Yi <hychou.svm@gmail.com>
2021-11-23 23:15:10 +0800

    API add POST /flow/job

--- app/api.py
+++ app/api.py
@@ -1,13 +1,14 @@
 import itertools
 from typing import Optional
 
-from core import parse_flow, save_flow
-from datatypes import TNodeTask
-from fastapi import FastAPI, File, UploadFile
+from fastapi import Depends, FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-
 from utensil.loopflow import loopflow
 from utensil.loopflow.functions import basic, dataflow
+
+from schema.datatypes import TNodeTask
+from schema.flow_job import FlowJobCreate, FlowJobInDb
+from service.core import FlowJobService, FlowService
 
 app = FastAPI()
 
@@ -57,7 +58,16 @@ async def query_list_node_tasks():
 
 
 @app.post("/parse-flow")
-async def query_parse_flow(file: UploadFile = File(...)):
-    file_id = save_flow(file)
-    flow_id, tflow = parse_flow(file_id)
+async def query_parse_flow(file: UploadFile = File(...),
+                           flow_service: FlowService = Depends(FlowService)):
+    file_id = flow_service.save_flow(file)
+    flow_id, tflow = flow_service.parse_flow(file_id)
     return {"flow_id": flow_id, "flow": tflow}
+
+
+@app.post("/flow/job", response_model=FlowJobInDb)
+async def create_flow_job(
+        flow_job_create: FlowJobCreate,
+        flow_job_service: FlowJobService = Depends(FlowJobService),
+):
+    return flow_job_service.create_flow_job(flow_job_create)
--- app/dao.py
+++ app/dao.py
@@ -1,38 +1,38 @@
-import pymongo
 from bson.objectid import ObjectId
-from datatypes import TFlow
-import os
 
-MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb://localhost:27017')
-client = pymongo.MongoClient(MONGODB_URL)
-db = client['loopflow-app']
+from database import DBSessionMixin
+from schema.datatypes import TFlow
+from schema.flow_job import FlowJob, FlowJobInDb
 
 
-class Dao:
-    pass
-
-
-class FlowFileDao(Dao):
+class FlowFileDao(DBSessionMixin):
 
     def save(self, file_name: str, file_content: str):
-        file_id = db.flowfile.insert_one({
+        file_id = self.db.flowfile.insert_one({
             "name": file_name,
             "content": file_content,
         }).inserted_id
         return str(file_id)
 
     def get(self, file_id):
-        return db.flowfile.find_one({"_id": ObjectId(file_id)})
+        return self.db.flowfile.find_one({"_id": ObjectId(file_id)})
 
 
-class FlowDao(Dao):
+class FlowDao(DBSessionMixin):
 
     def save(self, file_id: str, flow: TFlow):
-        flow_id = db.flow.insert_one({
+        flow_id = self.db.flow.insert_one({
             "file_id": ObjectId(file_id),
             "flow": flow.dict()
         }).inserted_id
         return str(flow_id)
 
     def get(self, flow_id):
-        return db.flow.find_one({"_id": ObjectId(flow_id)})
+        return self.db.flow.find_one({"_id": ObjectId(flow_id)})
+
+
+class FlowJobDao(DBSessionMixin):
+
+    def create(self, flow_job: FlowJob):
+        inserted_id = self.db.flowjob.insert_one(flow_job.dict()).inserted_id
+        return FlowJobInDb(**flow_job.dict(), job_id=str(inserted_id))
